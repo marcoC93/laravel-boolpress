b10937233a627c461995365b3f0967b602664e89
Marco <marco.chianese93@gmail.com>
2021-01-25 16:36:35 +0100

    creazione Faker (da sistemare)

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
 
 class PostTableSeeder extends Seeder
 {
@@ -9,8 +11,16 @@ class PostTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $posts = config('posts');
+        
+        for ($i=0; $i <10 ; $i++) {
+            $newPost= new Post();
+            $newPost->author = $faker->name;
+            $newPost->length = $faker->length;
+            $newPost->prova = $faker->prova;
+            $newPost->save();
+        }
     }
 }
